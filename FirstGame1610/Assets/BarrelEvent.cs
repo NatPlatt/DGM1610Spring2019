b095f577b6631d26a1f06ea2cb513a59e8670ffc
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-01-31 17:42:07 -0700

    Added sprites, physics, particle FX and scripts
    added physics to barrel top, flour, as well as particle FX and wrote scripts for each

---
+++ FirstGame1610/Assets/BarrelEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BarrelEvent : MonoBehaviour
+{
+	public UnityEvent Event;
+
+	private void OnCollisionEnter2D()
+	{
+		Event.Invoke();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
