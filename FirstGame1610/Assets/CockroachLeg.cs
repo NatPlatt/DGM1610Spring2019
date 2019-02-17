91b382b3e71d10ae9698d155792e17bb6a632794
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-02-17 15:10:43 -0700

    added script to cockroach legs

---
+++ FirstGame1610/Assets/CockroachLeg.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CockroachLeg : MonoBehaviour
+{
+    public Color legColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = legColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
