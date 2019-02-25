cfc1267b6afb7e5ccbcb0c2cc5128ec23fa40671
NatPlatt <n.zollinger26@hotmail.com>
2019-02-25 11:31:23 -0700

    started lamppost script for lightswitch flame

---
+++ FirstGame1610/Assets/Scripts/LampPost.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampPost : MonoBehaviour
+{
+    public bool LightSwitch;
+    private Sprite lampFlame;
+    private SpriteRenderer sr;
+    public GameObject flamePrefab;
+   
+    void Update()
+    {
+        if (LampPost)
+        {
+            GameObject go = Instantiate(Resources.Load("flamePrefab"")) as GameObject;
+        }
+        
+    }
+
+    private void Start()
+    {
+        lampFlame = Sprite.Create();
+    }
+}
