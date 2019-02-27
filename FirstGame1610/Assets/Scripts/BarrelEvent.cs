e26732080f35e0d412ebb5b1a96396d33de3274b
NatPlatt <n.zollinger26@hotmail.com>
2019-02-27 11:17:21 -0700

    added player controller to barrel

--- FirstGame1610/Assets/Scripts/BarrelEvent.cs
+++ FirstGame1610/Assets/Scripts/BarrelEvent.cs
@@ -6,15 +6,31 @@ using UnityEngine.Events;
 public class BarrelEvent : MonoBehaviour
 {
 	public UnityEvent Event;
+	public UnityEvent OnGrounded;
+	private CharacterController Controller;
+	private Vector3 position;
+	public float Speed;
+
+	private void Start()
+	{
+		Controller = GetComponent<CharacterController>();
+	}
 
 	private void OnCollisionEnter2D()
 	{
 		Event.Invoke();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Update()
+	{
+		if (Controller.isGrounded)
+		{
+			OnGrounded.Invoke();
+		}
+		position.y = Input.GetAxis("Vertical")*Speed*Time.deltaTime;
+		position.y -= 1; //added for gravity
+		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+		Controller.Move(position);
 	}
 }
 
