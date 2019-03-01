b31a12d9b6d0ca4dcb8a1ba77f5478a88ca7ea01
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-02-28 21:10:22 -0700

    tested instantiation with brick wall test

---
+++ FirstGame1610/Assets/Scripts/Instantiation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform brick;
+    void Start()
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
