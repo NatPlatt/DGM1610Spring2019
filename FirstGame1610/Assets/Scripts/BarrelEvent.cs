96e725b7fb575c35484594fa8d6436cb3c054d17
NatPlatt <n.zollinger26@hotmail.com>
2019-03-04 11:27:40 -0700

    added character controller movement
    added to barrel top and broom

--- FirstGame1610/Assets/Scripts/BarrelEvent.cs
+++ FirstGame1610/Assets/Scripts/BarrelEvent.cs
@@ -10,6 +10,7 @@ public class BarrelEvent : MonoBehaviour
 	private CharacterController Controller;
 	private Vector3 position;
 	public float Speed;
+	public float Gravity;
 
 	private void Start()
 	{
@@ -27,9 +28,19 @@ public class BarrelEvent : MonoBehaviour
 		{
 			OnGrounded.Invoke();
 		}
-		position.y = Input.GetAxis("Vertical")*Speed*Time.deltaTime;
-		position.y -= 1; //added for gravity
+
+		if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+		{
+			position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+		}
+		else
+		{
+			position.y = Gravity * Time.deltaTime; 
+		}
+
 		position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+		
+		
 		Controller.Move(position);
 	}
 }
