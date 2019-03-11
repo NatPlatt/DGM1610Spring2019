bfed2e858ab7a93e7d175a6fa4bfb33a81e90ea7
NatPlatt <n.zollinger26@hotmail.com>
2019-03-11 11:58:52 -0600

    added scripts for health bar
    still need health bar sprite

---
+++ FirstGame1610/Assets/Scripts/updateBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class updateBar : MonoBehaviour
+{
+    private Image barImage;
+
+    public FloatData fillNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        barImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        barImage.fillAmount = fillNumber.Value;
+    }
+}
