a39aac22df413f27ea312138b0725250f56f5fa5
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-03-12 20:38:19 -0600

    added scripts to cockroach legs
    to make them change color with key inputs. Getting errors from pigeon body and wing saying they are looking for character controller

--- FirstGame1610/Assets/Scripts/CockroachLeg.cs
+++ FirstGame1610/Assets/Scripts/CockroachLeg.cs
@@ -14,6 +14,19 @@ public class CockroachLeg : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
     }
 }
