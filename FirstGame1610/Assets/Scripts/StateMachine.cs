fb9663a55a4a7e28502ade2d38bdb07e54132db4
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-04-01 18:19:56 -0600

    added switch scripts
    both in class game states machine and weapons scripts

---
+++ FirstGame1610/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class StateMachine : ScriptableObject
+{
+    public UnityEvent StartingEvent, PlayingEvent, EndingEvent;
+    public enum States
+    {
+        Starting,
+        Playing,
+        Ending
+    }
+
+    public States CurrentStates;
+
+    public void ChangeToStarting()
+    {
+        CurrentStates = States.Starting;
+    }
+
+    public void ChangeToPlaying()
+    {
+        CurrentStates = States.Playing;
+    }
+
+    public void ChangeToEnding()
+    {
+        CurrentStates = States.Ending;
+    }
+    
+    public void OnSwitch()
+    {
+        switch (CurrentStates)
+        {
+            case States.Starting:
+                StartingEvent.Invoke();
+                break;
+            case States.Playing:
+                PlayingEvent.Invoke();
+                break;
+            case States.Ending:
+                EndingEvent.Invoke();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ FirstGame1610/Assets/Scripts/WeaponSwitches.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WeaponSwitches : MonoBehaviour
+{
+    public UnityEvent ShootingEvent, LoadingEvent, EmptyEvent;
+    public enum States
+    {
+        Shooting,
+        Loading,
+        Empty
+        
+    }
+
+    public States CurrentState;
+    
+    void Update()
+    {
+        switch (CurrentState)
+        {
+            case States.Shooting:
+                ShootingEvent.Invoke();
+                break;
+            case States.Loading:
+                LoadingEvent.Invoke();
+                break;
+            case States.Empty:
+                EmptyEvent.Invoke();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+       
+    }
+}
