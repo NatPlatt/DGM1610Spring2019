1be0db80fb889d8b08dca7408012f4042456bcfa
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-04-02 14:57:31 -0600

    added instantiation script to cockroach

--- FirstGame1610/Assets/Scripts/Cockroach.cs
+++ FirstGame1610/Assets/Scripts/Cockroach.cs
@@ -2,12 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cockroach : Insect {
+public class Cockroach : Insect
+{
+	public Transform roachLegs;
 
 	// Use this for initialization
 	void Start () {
 		
 		GetComponent<SpriteRenderer>().color = SkinColor;
+		
+		for (int y = 0; y < 2; y++)
+                {
+                    for (int x = 0; x < 2; x++)
+                    {
+                        Instantiate(roachLegs, new Vector3(x, y, 0), Quaternion.identity);
+                    }
+                }
+		
 	}
 	
 	// Update is called once per frame
