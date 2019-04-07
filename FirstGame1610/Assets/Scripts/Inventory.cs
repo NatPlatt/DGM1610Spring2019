e99cf4cc2933304dca6eceb94ae82ffe8ba80065
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-04-06 21:36:02 -0600

    added inventory scripts

---
+++ FirstGame1610/Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+
+        public Stuff(int bul)
+        {
+            bullets = bul;
+        }
+
+        public Stuff()
+        {
+            bullets = 1;
+        }
+    }
+    //creating an instance (an object) of the Stuff class
+    public Stuff myStuff = new Stuff(50);
+    
+
+    private void Start()
+    {
+        Debug.Log(myStuff.bullets);
+    }
+}
