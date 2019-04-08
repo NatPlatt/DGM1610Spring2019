13ec3d776625994e589e6023bde4af57c080e72a
NatPlatt <n.zollinger26@hotmail.com>
2019-04-08 11:23:54 -0600

    added enemy scripts and flip scripts
    also added empty enemy object in scene, need to add art to it

---
+++ FirstGame1610/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform Player;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = Player.position;
+    }
+    //OR
+    //void OnTriggerEnter(Collider other)
+    //OR
+    //void OnTriggerStay(Collider other)
+}
---
+++ FirstGame1610/Assets/Scripts/FlipChar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipChar : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.Rotate(0, 180, 0);
+    }
+}
