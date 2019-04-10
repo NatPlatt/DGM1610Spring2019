5515c7d50121b113d41595482998c93c2c7ae0cd
NatPlatt <n.zollinger26@hotmail.com>
2019-04-10 11:02:34 -0600

    in class work

---
+++ FirstGame1610/Assets/Scripts/Counter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Counter : MonoBehaviour
+{
+    public int Value = 3, MinValue = 0;
+    public float WaitTime = 1;
+
+    public UnityEvent OnCountEvent, OnEndEvent;
+
+    public void StartCounter()
+    {
+        StartCoroutine(RunCounter());
+    }
+    
+    private IEnumerator RunCounter() //couroutine
+    {
+        var waitObject = new WaitForSeconds(WaitTime);
+        
+        while (Value > MinValue)
+        {
+            yield return waitObject;
+            print(Value);
+            Value--;
+        }
+
+        yield return waitObject;
+        OnEndEvent.Invoke();
+    }
+
+}
