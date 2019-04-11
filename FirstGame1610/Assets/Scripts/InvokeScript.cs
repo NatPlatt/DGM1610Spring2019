2ccfe01046682a22080380e575e132b15c6cdf52
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-04-11 10:57:47 -0600

    added invoke script
    it instantiates a bunch of capsules into the scene using a while loop

---
+++ FirstGame1610/Assets/Scripts/InvokeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeScript : MonoBehaviour
+{
+    public GameObject target;
+    public int CapsuleAmount = 10;
+    public int amountSpawned = 0;
+
+    void Start()
+    {
+        while (amountSpawned < CapsuleAmount)
+        {
+            Invoke("SpawnObject", 2);
+            amountSpawned++;
+        }
+
+        //CancelInvoke("SpawnObject");
+    }
+
+    void SpawnObject()
+    {
+        float x = Random.Range(-2.0f, 2.0f);
+        float z = Random.Range(-2.0f, 2.0f);
+        Instantiate(target, new Vector3(x, 2, z), Quaternion.identity);
+    }
+}
