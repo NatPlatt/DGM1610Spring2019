eabb1bff44ab9c778173520b036f56f1855386e2
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-04-12 21:15:47 -0600

    added conversation script with switch statements

---
+++ FirstGame1610/Assets/Scripts/Conversation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conversation : MonoBehaviour
+{
+    public int intelligence = 5;
+
+    void Greet()
+    {
+        switch (intelligence)
+        {
+            case 5:
+                print("Hello there!");
+                break;
+            case 4:
+                print("How dare you?!");
+                break;
+            case 3:
+                print("I forgot my pearls!");
+                break;
+            case 2: 
+                print("Run away!");
+                break;
+            case 1:
+                print("Let's go!");
+                break;
+            default:
+                print("Incorrect intelligence level.");
+                break;
+        }
+    }
+}
