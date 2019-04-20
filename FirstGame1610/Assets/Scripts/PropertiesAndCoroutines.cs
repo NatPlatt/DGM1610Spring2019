f5fc2afd01a1179136c6b92466400373b791731e
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-04-19 20:07:10 -0600

    tried some scripts I learned, didn't work

--- FirstGame1610/Assets/Scripts/PropertiesAndCoroutines.cs
+++ FirstGame1610/Assets/Scripts/PropertiesAndCoroutines.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Properties : MonoBehaviour
+public class PropertiesAndCoroutines : MonoBehaviour
 {
     public float smoothing = 7f;
 
