bd0b1eee84b5fbc75831654471b4c8231f8ea4d2
Natalie Zollinger Platt <n.zollinger26@hotmail.com>
2019-04-26 21:25:31 -0600

    organized scene and wrote change color script

---
+++ FirstGame1610/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public Color changeColor;
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = changeColor;
+    }
+
+    
+}
